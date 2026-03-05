3e5bae981977a74c8a941e0c4d974b9d6ad66641
carlossantos01 <carlossantosgithub@gmail.com>
2026-03-05 16:45:56 -0300

    feat: final

---
+++ 11. Funcao.js
@@ -0,0 +1,31 @@
+// Função comum no Javascript
+
+function somar(a, b) {
+    return a + b
+}   
+
+function subtrair(a, b) {
+    return a - b
+} 
+function multiplicar(a, b) {
+    return a * b
+}
+function dividir(a, b) {
+    if (b === 0 ) {
+        console.log("Não é possível dividir por zero")
+        return null
+    } 
+    else {
+        return a / b
+    }
+}
+
+const soma = somar(2, 3)
+const subtracao = subtrair(2, 3)
+const multiplicacao = multiplicar(2, 3)
+const divisao = dividir(2, 3)
+
+console.log("Soma: ", soma)
+console.log("Subtração: ", subtracao)   
+console.log("Multiplicação: ", multiplicacao)
+console.log("Divisão: ", divisao)
---
+++ 9. operadores - I .js
@@ -0,0 +1,24 @@
+// Operados lógicos
+
+let idade = 10
+let peso = "10"
+
+
+console.log("- COMPARAÇÃO -")
+console.log("Comparando conteúdo: ")
+console.log("Idade e peso são iguais?")
+console.log(idade == peso) // compara o conteúdo
+
+
+console.log("\nComparando e conteúdo: ")
+console.log("Idade e peso são iguais?")
+console.log(idade === peso) // compara o conteúdo e o tipo
+
+console.log("- DIFERENÇA -")
+console.log("\nComparando conteúdo: ")
+console.log("Idade e peso são diferentes?")
+console.log(idade != peso) // compara o conteúdo
+
+console.log("\nComparando tipo e conteúdo: ")
+console.log("Idade e peso são diferentes?")
+console.log(idade !== peso) // compara o conteúdo e o tipo    
